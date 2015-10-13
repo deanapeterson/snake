e34480c096c5af4dd2a9a9a8ace51553c32a3a27
Dean Peterson <dean.peterson.iconweb@gmail.com>
2015-10-12 22:58:23 -0500

    =

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var copy = require('gulp-copy');
+var data = require('gulp-data');
+var rimraf = require('gulp-rimraf');
+var config = {
+  dir : {
+    build : 'target/'
+  },
+  files : {
+    srcJs : [
+      'src/**/*.module.js',
+      'src/**/*.js'
+    ]
+  }
+}
+
+gulp.task('clean', function(cb){
+  gulp.src(config.dir.build)
+    .pipe(rimraf());
+    cb();
+});
+
+gulp.task('copyJs', function(){
+  return gulp.src(config.files.srcJs).pipe(
+    copy(config.dir.build)
+  );
+
+})
+
+gulp.task('build',['clean', 'copyJs'], function(){
+
+
+
+});
+
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
---
+++ target/src/js/snake-byte.module.js
@@ -0,0 +1,1 @@
+angular.module('snake-byte', []);
